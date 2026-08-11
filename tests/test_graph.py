31ef08959fbb7f6e1dfc56975c18d12f80f5a643
CHARANKUMARCHUKKA <charan21003@gmail.com>
2026-08-11 15:02:48 +0530

    test(graph): add test for security group mapping and edge creation

--- tests/test_graph.py
+++ tests/test_graph.py
@@ -11,3 +11,9 @@ def test_subnet_mapping():
     engine.map_subnets()
     assert "sub-1" in engine.graph.nodes
     assert engine.graph.nodes["sub-1"]["type"] == "Subnet"
+
+def test_security_group_mapping():
+    engine = CloudTopologyEngine({"ec2": [], "subnets": [], "security_groups": [{"group_id": "sg-1", "ingress_rules": [{"port": 80, "source": "0.0.0.0/0"}]}]})
+    engine.map_security_groups()
+    assert "sg-1" in engine.graph.nodes
+    assert engine.graph.has_edge("0.0.0.0/0", "sg-1")
