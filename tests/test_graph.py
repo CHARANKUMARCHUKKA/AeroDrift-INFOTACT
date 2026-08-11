5b22ed2b9ae12ff3edb44ea0a9f312816216a75c
CHARANKUMARCHUKKA <charan21003@gmail.com>
2026-08-11 15:02:48 +0530

    test(graph): add test for accurate subnet mapping and node creation

--- tests/test_graph.py
+++ tests/test_graph.py
@@ -5,3 +5,9 @@ from graph_engine import CloudTopologyEngine
 def test_engine_initialization():
     engine = CloudTopologyEngine({"ec2": [], "subnets": [], "security_groups": []})
     assert isinstance(engine.graph, nx.DiGraph)
+
+def test_subnet_mapping():
+    engine = CloudTopologyEngine({"ec2": [], "subnets": [{"subnet_id": "sub-1", "route_table_id": "rtb-1"}], "security_groups": []})
+    engine.map_subnets()
+    assert "sub-1" in engine.graph.nodes
+    assert engine.graph.nodes["sub-1"]["type"] == "Subnet"
