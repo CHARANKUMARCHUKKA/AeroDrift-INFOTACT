bc740057ea8dba012cfeb1c7e5f4fb277acc2bbd
CHARANKUMARCHUKKA <charan21003@gmail.com>
2026-08-11 15:02:46 +0530

    test(ingest): add test for successful Subnet fetching

--- tests/test_ingestion.py
+++ tests/test_ingestion.py
@@ -7,3 +7,10 @@ async def test_ec2_fetching():
     instances = await fetch_ec2_instances()
     assert len(instances) == 2
     assert instances[0].instance_id == "i-0abcd1234efgh5678"
+
+from aws_ingestion import fetch_subnets
+@pytest.mark.asyncio
+async def test_subnet_fetching():
+    subnets = await fetch_subnets()
+    assert len(subnets) == 2
+    assert subnets[0].subnet_id == "subnet-111"
