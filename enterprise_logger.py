ca11f2b1946a20cf183f9318d6bbc6c9e583f02b
CHARANKUMARCHUKKA <charan21003@gmail.com>
2026-08-15 20:56:30 +0530

    feat: configure file rotating handler for persistent logs

--- enterprise_logger.py
+++ enterprise_logger.py
@@ -16,4 +16,20 @@ class JSONFormatter(logging.Formatter):
 def setup_enterprise_logger(name):
     logger = logging.getLogger(name)
     logger.setLevel(logging.INFO)
+    
+    if logger.handlers:
+        return logger
+        
+    os.makedirs("logs", exist_ok=True)
+    from logging.handlers import RotatingFileHandler
+    file_handler = RotatingFileHandler("logs/aerodrift.json", maxBytes=5*1024*1024, backupCount=3)
+    file_handler.setFormatter(JSONFormatter())
+    
+    console_handler = logging.StreamHandler()
+    console_handler.setFormatter(logging.Formatter('%(asctime)s - %(levelname)s - %(message)s'))
+    
+    logger.addHandler(file_handler)
+    logger.addHandler(console_handler)
+    logger.propagate = False 
+    
     return logger
