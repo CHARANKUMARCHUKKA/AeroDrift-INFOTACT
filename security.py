d6aa590180bd480b42e3e59683cb5db87473ca94
CHARANKUMARCHUKKA <charan21003@gmail.com>
2026-08-25 12:24:05 +0530

    feat: implement JWT token generation logic

--- security.py
+++ security.py
@@ -7,3 +7,17 @@ def verify_password(plain_password, hashed_password):
 
 def get_password_hash(password):
     return pwd_context.hash(password)
+
+from datetime import datetime, timedelta
+from jose import jwt
+
+SECRET_KEY = "aerodrift_super_secret_enterprise_key"
+ALGORITHM = "HS256"
+ACCESS_TOKEN_EXPIRE_MINUTES = 30
+
+def create_access_token(data: dict):
+    to_encode = data.copy()
+    expire = datetime.utcnow() + timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
+    to_encode.update({"exp": expire})
+    encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
+    return encoded_jwt
